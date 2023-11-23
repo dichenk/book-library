97e5af79c778384f31974a9abcdb26a615f72d67
Oleg Diachenko <diachenko.oleg@proton.me>
2023-11-24 01:07:32 +0300

    closed access to the get list of users

--- app_user/views.py
+++ app_user/views.py
@@ -2,11 +2,16 @@ from rest_framework import generics
 from .models import Customer
 from .serializers import CustomerSerializer
 from .tasks import send_welcome_email
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class UserList(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    def get(self, request, *args, **kwargs):
+        return Response({"message": "Access to the list of users is forbidden"}, status=status.HTTP_403_FORBIDDEN)
 
     def perform_create(self, serializer):
         user = serializer.save()
